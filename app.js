3207d5f00ee3369021753175962776d302c50055
LPT <lptalty@gmail.com>
2021-03-20 23:17:41 -0400

    I added a lot of functionality to the calculator as well as styled it with CSS. I had to look at a few edge cases such as dividing by zero and the user continually trying to hit the decimal button. Once there is an error displayed on the screen, the user has to hit AC to restart. The project is mostly done; although, when the number is quite large, I have set the overflow to hidden, which will display the incorrect numbers once they are too big. This only happened when I maxed out the numbers and would never happen if the user was using the calculator normally. All the other cases should be accounted for. I realized I could put in a button for the user to delete an incorrect click, but I don't think it would look good on my overall layout. I will likely not include it. If I continue working on this project, I will have to fix the very large numbers that are currently being hidden within the display div.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //creating four functions: add, subtract, multiply, divide
 
 function add(a, b){
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b){
@@ -27,15 +27,112 @@ function operate(num1, num2, operator){
 
 const myNumbers = document.getElementsByClassName("number")
 const clearBtn = document.getElementById("clearBtn")
+const equalBtn = document.getElementById("equalBtn")
+const myOperators = document.getElementsByClassName("operator")
+const decimalBtn = document.getElementById("decimal")
+const plusMinusBtn = document.getElementById("plusMinus")
+
+let num1 = undefined;
+let num2 = undefined;
+let currentOperator = undefined;
+let result = undefined;
+let operated = false;
+let decimalClick = false;
 
 for(let number of myNumbers){
     number.addEventListener('click', function(){
-        document.getElementById("display").innerText += number.innerText
+        if(document.getElementById("display").innerText != 'Err'){
+            if(num1 != undefined && num2 === undefined){
+                if((document.getElementById("display").innerText !== '-') && (document.getElementById("display").innerText !== '.')){
+                    document.getElementById("display").innerText = ''
+                } 
+                document.getElementById("display").innerText += number.innerText
+                num2 = number.innerText
+                operated = false;
+            } else if (num1 != undefined && num2 != undefined){
+                document.getElementById("display").innerText += number.innerText
+            } else if(num1 === undefined){
+                document.getElementById("display").innerText += number.innerText
+            }
+        }
     })
 }
 
 clearBtn.addEventListener('click', function(){
     document.getElementById("display").innerText = '';
+    num1 = undefined;
+    num2 = undefined;
+    currentOperator = undefined;
+    decimalClick = false;
 })
+
+equalBtn.addEventListener('click', function(){
+    num2 = document.getElementById("display").innerText;
+    if( num1 ==='0' && num2 === '0' && currentOperator === '/'){
+        document.getElementById("display").innerText = 'Err';
+        num1 = undefined;
+        num2 = undefined;
+        currentOperator = undefined;
+    } else if(num1 !=undefined && num2 != undefined && currentOperator != undefined){
+        result = operate(num1, num2, currentOperator)
+        document.getElementById("display").innerText = Math.round(result * 100) / 100;
+        num1 = undefined;
+        num2 = undefined;
+        currentOperator = undefined;
+        
+    }
+})
+
+decimalBtn.addEventListener('click', function(){
+    if(document.getElementById("display").innerText != 'Err'){
+        if(decimalClick === false){
+            decimalClick = true;
+            document.getElementById("display").innerText += '.'
+        }
+    }
+})
+
+plusMinusBtn.addEventListener('click', function(){
+    if(document.getElementById("display").innerText !== '' && document.getElementById("display").innerText !== '-' ){
+        document.getElementById("display").innerText *= -1; 
+    } else if(document.getElementById("display").innerText === ''){
+        document.getElementById("display").innerText = '-'; 
+    } else if (document.getElementById("display").innerText === '-'){
+        document.getElementById("display").innerText = ''; 
+    }
+})
+
+for(let operator of myOperators){
+    operator.addEventListener('click', function(){
+        if(document.getElementById("display").innerText != 'Err'){
+            if(currentOperator != undefined && operated === false){
+                num2 = document.getElementById("display").innerText;
+    
+                if( num1 ==='0' && num2 === '0' && currentOperator === '/'){
+                    document.getElementById("display").innerText = 'Err';
+                    num1 = undefined;
+                    num2 = undefined;
+                    currentOperator = undefined;
+                } else {
+                    result = operate(num1, num2, currentOperator)
+                    document.getElementById("display").innerText = Math.round(result * 100) / 100;
+                    decimalClick = false;
+                    num1 = Math.round(result * 100) / 100;
+                    num2 = undefined;
+                    operated = true;
+                    currentOperator = operator.innerText;
+                }
+            } else if(currentOperator != undefined && operated === true){
+                currentOperator = operator.innerText;
+            } else if(currentOperator === undefined){
+                currentOperator = operator.innerText
+                num1 = document.getElementById("display").innerText;
+                document.getElementById("display").innerText = '';
+                decimalClick = false;
+            } 
+        }
+        })
+    }
+
 
 
